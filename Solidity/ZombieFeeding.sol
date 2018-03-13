3e1ca96fac4d30ceb7d60aecca3d1348ea92474d
HUYNHBINH\huynh <huynhbinh0511@gmail.com>
2018-03-14 00:11:48 +0700

    commit  -  finish merge zombie dna and cat dna, to create a new zombie cat

--- Solidity/ZombieFeeding.sol
+++ Solidity/ZombieFeeding.sol
@@ -2,9 +2,8 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
-// Create KittyInterface here
 contract KittyInterface {
-    function getKitty(uint256 _id) external view returns (
+  function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
     uint256 cooldownIndex,
@@ -15,17 +14,33 @@ contract KittyInterface {
     uint256 sireId,
     uint256 generation,
     uint256 genes
-);
+  );
 }
 
 contract ZombieFeeding is ZombieFactory {
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  // Modify function definition here:
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Add an if statement here
+    if(keccak256 (_species) == "kitty")
+    {
+        newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    // And modify function call here:
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
