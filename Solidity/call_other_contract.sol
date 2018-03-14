b61d5cf5e1c456e85a7f920c9c90fd6e75f0e44d
HUYNHBINH\huynh <huynhbinh0511@gmail.com>
2018-03-14 15:32:59 +0700

    commit - add crypto kitty constructor for reference, add web3js to interact with the smart contract

--- Solidity/call_other_contract.sol
+++ Solidity/call_other_contract.sol
@@ -18,3 +18,33 @@ contract MyContract {
 }
 
 In this way, your contract can interact with any other contract on the Ethereum blockchain, as long they expose those functions as public or external.
+
+
+
+// Crypto kitty contrsctor
+function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+) {
+    Kitty storage kit = kitties[_id];
+
+    // if this variable is 0 then it's not gestating
+    isGestating = (kit.siringWithId != 0);
+    isReady = (kit.cooldownEndBlock <= block.number);
+    cooldownIndex = uint256(kit.cooldownIndex);
+    nextActionAt = uint256(kit.cooldownEndBlock);
+    siringWithId = uint256(kit.siringWithId);
+    birthTime = uint256(kit.birthTime);
+    matronId = uint256(kit.matronId);
+    sireId = uint256(kit.sireId);
+    generation = uint256(kit.generation);
+    genes = kit.genes;
+}
