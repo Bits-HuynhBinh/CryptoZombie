f610b1570722818fed699e62c67e6a2ef86a5285
HUYNHBINH\huynh <huynhbinh0511@gmail.com>
2018-03-15 11:44:57 +0700

    commit - add feature get zombies by owner address

--- Solidity/ZombieHelper.sol
+++ Solidity/ZombieHelper.sol
@@ -9,8 +9,7 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
-  // Start here
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId){
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   }
@@ -18,6 +17,19 @@ contract ZombieHelper is ZombieFeeding {
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns(uint[]) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    // Start here
+    uint counter = 0;
+    for(uint i = 0; i < zombies.length; i++){
+      if(zombieToOwner[i] == _owner){
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
   }
 
 }
